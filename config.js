9626a1a216522f4c2f130e03dfcb9ee265ab7da2
theonlyamos <theonlyamos@gmail.com>
2021-03-26 13:52:01 +0000

    stripe live

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     'secretKey': process.env.HERMESCRAFT_SECRET_KEY,
     'mongoUrI': process.env.HERMESCRAFT_MONGO_URI,
-    'stripeSecretKey': process.env.HERMESCRAFT_STRIPE_SECRET_KEY,
-    'stripePublicKey': process.env.HERMESCRAFT_STRIPE_PUBLIC_KEY,
+    'stripeSecretKey': process.env.HERMESCRAFT_STRIPE_LIVE_SECRET_KEY,
+    'stripePublicKey': process.env.HERMESCRAFT_STRIPE_LIVE_PUB_KEY,
     'googleApiKey': process.env.HERMESCRAFT_GOOGLE_API_KEY,
     'hermescraftUrl': process.env.HERMESCRAFT_URL,
     'hermescraftAdminUrl': process.env.HERMESCRAFT_ADMIN_URL
